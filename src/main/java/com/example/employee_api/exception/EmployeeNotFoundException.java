0748c545da80c93f9fcbff40dba99dfdccd9b90d
anol123 <anol.roy123@gmail.com>
2026-03-22 01:16:10 +0530

    Added repository layer

--- src/main/java/com/example/employee_api/exception/EmployeeNotFoundException.java
+++ src/main/java/com/example/employee_api/exception/EmployeeNotFoundException.java
@@ -1,4 +1,7 @@
 package com.example.employee_api.exception;
 
-public class EmployeeNotFoundException {
+public class EmployeeNotFoundException extends RuntimeException {
+    public EmployeeNotFoundException(Long id) {
+        super("Employee not found with id: " + id);
+    }
 }
--- src/main/java/com/example/employee_api/repository/EmployeeRepository.java
+++ src/main/java/com/example/employee_api/repository/EmployeeRepository.java
@@ -1,4 +1,16 @@
 package com.example.employee_api.repository;
 
-public class EmployeeRepository {
+import com.example.employee_api.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+    Optional<Employee> findByEmail(String email);
+    List<Employee> findByDepartment(String department);
+    List<Employee> findBySalaryGreaterThan(Double salary);
+    boolean existsByEmail(String email);
 }
