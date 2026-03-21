9e62bff3eb7a763a489b7158ebca600ba294dbe0
anol123 <anol.roy123@gmail.com>
2026-03-22 01:00:52 +0530

    Added Dtos

--- src/main/java/com/example/employee_api/dto/EmployeeRequestDto.java
+++ src/main/java/com/example/employee_api/dto/EmployeeRequestDto.java
@@ -1,4 +1,23 @@
 package com.example.employee_api.dto;
 
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+@Data
 public class EmployeeRequestDto {
+
+    @NotBlank(message="Name is required")
+    @Size(min=2,max=100, message="Name must be between 2 and 100 characters")
+    private String name;
+
+    @NotBlank(message="Email id required")
+    @Email(message="Must be a valid email address")
+    private String email;
+
+    @NotBlank(message="Department is required")
+    private String department;
+
+    @NotNull(message="Salary  is required")
+    @Positive(message="Salary must be a positive number")
+    private Double salary;
 }
--- src/main/java/com/example/employee_api/dto/EmployeeResponseDto.java
+++ src/main/java/com/example/employee_api/dto/EmployeeResponseDto.java
@@ -1,4 +1,15 @@
 package com.example.employee_api.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class EmployeeResponseDto {
+    private Long id;
+    private String name;
+    private String email;
+    private String department;
+    private Double salary;
+
 }
