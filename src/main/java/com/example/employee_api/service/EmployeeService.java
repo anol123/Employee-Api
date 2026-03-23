b889907140c40388644b280036d87cb97ac089d1
anol123 <anol.roy123@gmail.com>
2026-03-23 23:25:39 +0530

    writing service

--- src/main/java/com/example/employee_api/service/EmployeeService.java
+++ src/main/java/com/example/employee_api/service/EmployeeService.java
@@ -1,4 +1,25 @@
 package com.example.employee_api.service;
 
+import com.example.employee_api.dto.EmployeeRequestDto;
+import com.example.employee_api.dto.EmployeeResponseDto;
+import com.example.employee_api.entity.Employee;
+import com.example.employee_api.repository.EmployeeRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmployeeService {
+    private EmployeeRepository employeeRepository;
+
+
+    private EmployeeResponseDto toResponseDto(Employee employee){
+        return EmployeeResponseDto.builder()
+                .id(employee.getId())
+                .name(employee.getName())
+                .email(employee.getEmail())
+                .department(employee.getDepartment())
+                .salary(employee.getSalary())
+                .build();
+    }
+
+
 }
