d231158ef2525eee0e9248b89fadda5deca941d6
anol123 <anol.roy123@gmail.com>
2026-03-26 22:11:07 +0530

    added getAllEmployee method

--- src/main/java/com/example/employee_api/service/EmployeeService.java
+++ src/main/java/com/example/employee_api/service/EmployeeService.java
@@ -4,9 +4,14 @@ import com.example.employee_api.dto.EmployeeRequestDto;
 import com.example.employee_api.dto.EmployeeResponseDto;
 import com.example.employee_api.entity.Employee;
 import com.example.employee_api.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
+@RequiredArgsConstructor
 public class EmployeeService {
     private EmployeeRepository employeeRepository;
 
@@ -27,9 +32,21 @@ public class EmployeeService {
             throw new IllegalArgumentException("Email already exists: "+ requestDto.getEmail());
         }
 
-        Employee employee = Employee.builder().name(requestDto.getName()).email(requestDto.getEmail()).department(requestDto.getDepartment()).salary(requestDto.getSalary()).build();
+        Employee employee = Employee.builder()
+                .name(requestDto.getName())
+                .email(requestDto.getEmail())
+                .department(requestDto.getDepartment())
+                .salary(requestDto.getSalary())
+                .build();
         Employee saved = employeeRepository.save(employee);
         return toResponseDto(saved);
+    }
+
+    public List<EmployeeResponseDto> getAllEmployees(){
+        return employeeRepository.findAll()
+                .stream()
+                .map(this::toResponseDto)
+                .collect(Collectors.toList());
     }
 
 
