25fead470218c8e1bcc01d5c4891ca6dd5159ac1
anol123 <anol.roy123@gmail.com>
2026-03-27 20:41:48 +0530

    added one method in controller

--- src/main/java/com/example/employee_api/controller/EmployeeController.java
+++ src/main/java/com/example/employee_api/controller/EmployeeController.java
@@ -1,8 +1,19 @@
 package com.example.employee_api.controller;
 
+import com.example.employee_api.dto.EmployeeResponseDto;
+import com.example.employee_api.service.EmployeeService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class EmployeeController {
+import java.util.List;
 
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+    private EmployeeService employeeService;
+    @RequestMapping("/employee-list")
+    public ResponseEntity<List<EmployeeResponseDto>> getAllEmployees(){
+        return ResponseEntity.ok(employeeService.getAllEmployees());
+    }
 }
